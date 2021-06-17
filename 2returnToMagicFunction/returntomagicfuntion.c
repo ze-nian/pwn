30839d1f787b30d12dccd534f0fa9fb4d40c183d
ze-nian <63274855+ze-nian@users.noreply.github.com>
2021-06-17 23:30:30 +0800

    Add files via upload

---
+++ 2returnToMagicFunction/returntomagicfuntion.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include <unistd.h>
+
+int magicfuntion()
+{
+system("/bin/sh");
+return 1;
+}
+
+int function(int a)
+{
+char buffer[150];
+read(STDIN_FILENO,buffer,250);
+return a;
+}
+
+int main()
+{
+function(1);
+write(STDOUT_FILENO,"Hi\n",3);
+return 1;
+}
